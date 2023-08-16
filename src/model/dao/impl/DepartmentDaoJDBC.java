0f63964bc41eda6158a0c732ed47b01e447c1a71
David Raposo <92717795+Raposo06@users.noreply.github.com>
2023-08-16 19:37:36 +0100

    DepartmentDao implementation findAll

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,85 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDAO;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class DepartmentDaoJDBC implements DepartmentDAO{
+	
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn){
+		this.conn = conn;
+	}
+	
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try{
+			st = conn.prepareStatement(
+					"SELECT * FROM department ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List<Department> list = new ArrayList<>();
+			
+			while(rs.next()){
+				Department dep = instatiateDeparment(rs);
+				list.add(dep);
+			}
+			return list;
+			
+		}catch (SQLException e){
+			throw new DbException(e.getMessage());
+		}finally{
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+	
+	private Department instatiateDeparment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+
+		return dep;
+	}
+
+}
